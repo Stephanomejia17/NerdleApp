e9f01834ac57b08a6df5c6257246c173cf5ff1b0
Stephanomejia17 <stephano.mejia@outlook.es>
2023-09-19 20:50:41 -0500

    Implementacion de método generar_ecuacion, la cual retorna una ecuacion aleatoria

--- Clases/Clases.py
+++ Clases/Clases.py
@@ -7,8 +7,19 @@ class Jugador:
     pass
 
 class Ecuacion:
-    pass
+    def __init__(self):
+        self.ecuaciones: list = []
 
+    def generar_ecuacion(self) -> str:
+        alea = random.randint(0, 780)
 
+        # Abre el archivo Notepad
+        with open('requirements/ecuaciones.txt', 'r') as archivo:
+            # Lee todas las líneas del archivo y las almacena en una lista
+            lineas = archivo.readlines()
 
+        # Procesa las líneas para eliminar caracteres de nueva línea ('\n') y crea una lista
+        self.ecuaciones = [linea.strip() for linea in lineas]
 
+        ecuacion = self.ecuaciones.pop(alea)
+        return ecuacion
