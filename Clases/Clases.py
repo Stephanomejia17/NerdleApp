6189188edae9b680f1e5c51ac9141cd9e4fac66d
Samuel Duran <samuelduran0705@gmail.com>
2023-09-21 23:27:13 -0500

    creación del tablero

--- Clases/Clases.py
+++ Clases/Clases.py
@@ -1,6 +1,19 @@
 import random
 
 class Nerdle:
+    def __init__(self):
+        self.tablero = self.crear_tablero()
+
+    def crear_tablero(self):
+        filas = 6
+        columnas = 8
+        tablero = [[None] * columnas for _ in range(filas)]
+        return tablero
+
+    def mostrar_tablero(self):
+        for fila in self.tablero:
+            print(fila)
+
     pass
 
 class Jugador:
