acdb06696c1172de107bf9ff23d7e1a21222c64a
Stephano <81366758+Stephanomejia17@users.noreply.github.com>
2023-09-21 23:07:01 -0500

    Delete Clases/matriz_tablero.py

--- Clases/matriz_tablero.py
+++
@@ -1,7 +0,0 @@
-# Crear el tablero con dimensiones 6x8
-tablero = [[' ' for _ in range(8)] for _ in range(6)]
-
-# Imprimir el tablero
-for fila in tablero:
-    print('|'.join(fila))
-    print('-' * 17)
