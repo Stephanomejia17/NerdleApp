0b3e0b3ba15963b10979ecbc91b2641ccf250a93
jpenuela420 <penuelaherrerajuanjose@gmail.com>
2023-09-21 23:32:35 -0500

    Creacion del menu principal del programa

--- interfaz/consola.py
+++ interfaz/consola.py
@@ -1,2 +1,19 @@
 class UIConsola:
-    pass
+    def __init__(self):
+        self.nerdle: Optional[Nerdle] = None
+        self.opciones = {
+            "1": self.iniciar_nuevo_juego,
+            "2": self.mostrar_reglas,
+            "3": self.mostrar_estadisticas,
+            "4": self.salir
+        }
+    @staticmethod
+    def mostrar_menu():
+        titulo = " Nerdle "
+        print(f"\n{titulo:_^30}")
+        print("1. Iniciar nuevo juego")
+        print("2. Mostrar reglas")
+        print("3. Mostrar estadisticas")
+        print("4. Salir")
+        print(f"{'_':_^30}")
+    mostrar_menu()
