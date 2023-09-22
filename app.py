3330e25c1c4bb6f97101143afccf94aff8a6dade
Stephanomejia17 <stephano.mejia@outlook.es>
2023-09-22 00:49:17 -0500

    Implementando arranque de app

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from Clases.Clases import *
+from interfaz.consola import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Ejecucion principal de código
-    ecuacion_1 = Ecuacion()
-    ecuacionU = input("Ingrese su ecuacion: ")
-    jugador_1 = Jugador(ecuacionU)
 
+    consola = UIConsola()
+    consola.ejecutar_app()
 
-    pass
