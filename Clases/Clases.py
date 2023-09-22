deb48bb13466b433b6f7ca7c83004f2db4896de7
Stephanomejia17 <stephano.mejia@outlook.es>
2023-09-21 21:03:12 -0500

    Implementacion de método comparar_ecuaciones

--- Clases/Clases.py
+++ Clases/Clases.py
@@ -3,12 +3,12 @@ import random
 class Nerdle:
     pass
 
-class Jugador:
-    pass
 
 class Ecuacion:
+    ecuaciones: list = []
+    ecuacion_alea: str
     def __init__(self):
-        self.ecuaciones: list = []
+        self.ecuacion: list[str] = []
 
     def generar_ecuacion(self) -> str:
         alea = random.randint(0, 780)
@@ -21,5 +21,24 @@ class Ecuacion:
         # Procesa las líneas para eliminar caracteres de nueva línea ('\n') y crea una lista
         self.ecuaciones = [linea.strip() for linea in lineas]
 
-        ecuacion = self.ecuaciones.pop(alea)
-        return ecuacion
+        ecuacion_aleatoria = self.ecuaciones.pop(alea)
+        return ecuacion_aleatoria
+
+    def comparar_ecuaciones(self, ecuacion_usuario: str) -> bool | list[str]:
+        if self.ecuacion_alea == ecuacion_usuario:
+            return True
+        else:
+            resultado: list[str] = ['0'] * 8
+            for n in range(0, 8):
+                if self.ecuacion_alea[n] == ecuacion_usuario[n]:
+                    resultado[n] = '2'  # si es igual
+                else:
+                    if self.ecuacion_alea[n] != ecuacion_usuario[n]:
+                        if ecuacion_usuario[n] in self.ecuacion_alea:
+                            resultado[n] = '1'
+        return resultado
+
+
+class Jugador:
+    def __int__(self):
+        self.ecuacion: Ecuacion = None
--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Ejecucion principal de código
+    ecuacion_1 = Ecuacion()
+    ecuacionU = input("Ingrese su ecuacion: ")
+    jugador_1 = Jugador(ecuacionU)
 
 
     pass
