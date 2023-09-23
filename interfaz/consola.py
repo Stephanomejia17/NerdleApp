9164df6a8022b4df38d9c7b9c7e3e1e89ffcdbf6
Stephanomejia17 <stephano.mejia@outlook.es>
2023-09-22 22:02:34 -0500

    Arreglos de Master

--- interfaz/consola.py
+++ interfaz/consola.py
@@ -21,22 +21,6 @@ class UIConsola:
         self.lista_de_intentos: list[str] = []
         self.ecuacion_generada: str = ''
         self.intentos: int = 0
-    @staticmethod
-    def imprimir_reglas():
-
-        print("Nerdle:\n\n"
-              "------------------------------------------------------------------------------\n"
-              "Resuelve la ecuación en seis intentos\n"
-              "- Componen la ecuación: números (0-9), operaciones básicas y el signo igual.\n"
-              "- Tienes 6 intentos para colocarlos y llegar al resultado correcto.\n"
-              f"- Los colores indican lo siguiente:\n"
-              f"  1. {color_verde}Verde{resetear_color} correcta\n"
-              f"  2. {color_amarillo} Amarillo {resetear_color} existe pero está en la posición incorrecta\n"
-              f"  3. {color_rojo} Rojo {resetear_color} no pertenece a la ecuación.\n"
-              "- Sigue el orden de operaciones y permite conmutabilidad en el resultado.\n"
-              "- El objetivo es resolverla en el menor número de intentos posible.\n\n"
-              "¡Buena suerte!\n"
-              "_______________________________________________________________________________")
         
     @staticmethod
     def mostrar_menu():
